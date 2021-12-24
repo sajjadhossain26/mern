16b1b75867ea29f5287c5b94b302848255ff8e7e
Sajjad <sajjadhossainctg26@gmail.com>
2021-12-24 21:17:49 +0600

    submit

---
+++ class11/app.js
@@ -0,0 +1,46 @@
+// const h1 = document.querySelector("h1");
+// h1.setAttribute("width", "50px");
+// h1.className = "hossain";
+
+// console.log(h1.getAttribute("width"));
+// if (h1.hasAttribute) {
+//   console.log("ace");
+// } else {
+//   console.log("nai");
+// }
+
+const team = document.querySelector(".team");
+devs.map((data) => {
+  team.innerHTML += `
+ 
+   <div class="team-item">
+      <img
+        src="${data.photo}"
+        alt=""
+      />
+      <h1>${data.name}</h1>
+      <h3>${data.skill}</h3>
+      <h3>${data.location}</h3>
+    </div>
+  
+`;
+});
+
+const phoneBook = document.querySelector(".phonebook .member tbody");
+familyPhone.map((data, index) => {
+  phoneBook.innerHTML += `
+ 
+      <tr>
+        <td>${index + 1}</td>
+        <td>${data.name}</td>
+        <td>${data.age}</td>
+        <td>${data.relation}</td>
+        <td>${data.phone}</td>
+        <td><img src="${data.photo}"></td>
+      </tr>
+      </thead>
+    </table>
+    
+  
+  `;
+});
---
+++ class11/data.js
@@ -0,0 +1,107 @@
+const devs = [
+  {
+    name: "Sajjad Hossain",
+    age: 20,
+    skill: "mern stack",
+    photo:
+      "https://img.freepik.com/free-photo/young-handsome-man-with-beard-isolated-keeping-arms-crossed-frontal-position_1368-132662.jpg?size=626&ext=jpg",
+    location: "Chittagong",
+  },
+  {
+    name: "Saimon",
+    age: 44,
+    skill: "Python",
+    photo:
+      "https://img.freepik.com/free-photo/young-handsome-man-with-beard-isolated-keeping-arms-crossed-frontal-position_1368-132662.jpg?size=626&ext=jpg",
+    location: "Dhaka",
+  },
+  {
+    name: "Selim",
+    age: 24,
+    skill: "Java",
+    photo:
+      "https://img.freepik.com/free-photo/handsome-confident-smiling-man-with-hands-crossed-chest_176420-18743.jpg?size=626&ext=jpg",
+    location: "Cox's bazar",
+  },
+  {
+    name: " Hossain",
+    age: 30,
+    skill: "PHP",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRNCDplD_v_1KfSNO0zhO-6nSru6ntFmQkNBH7HAFqi_LJbwX2ejLIBOKx7_3Ju97AU_s&usqp=CAU",
+    location: "Chittagong",
+  },
+  {
+    name: "Jasim",
+    age: 29,
+    skill: "Mongo db",
+    photo:
+      "https://image.freepik.com/free-photo/handsome-confident-smiling-man-with-hands-crossed-chest_176420-18743.jpg",
+    location: "cumilla",
+  },
+  {
+    name: "Kalam",
+    age: 40,
+    skill: "Javascript",
+    photo:
+      "https://image.freepik.com/free-photo/happy-confident-customer-recommending-new-product_1262-18240.jpg",
+    location: "Mirpur",
+  },
+];
+
+const familyPhone = [
+  {
+    serial: 1,
+    name: "Sajjad",
+    age: 20,
+    relation: "brother",
+    phone: "39843948793749",
+    photo:
+      "https://image.freepik.com/free-photo/happy-confident-customer-recommending-new-product_1262-18240.jpg",
+  },
+  {
+    serial: 1,
+    name: "Hossain",
+    age: 27,
+    relation: "father",
+    phone: "39843948793749",
+    photo:
+      "https://img.freepik.com/free-photo/handsome-confident-smiling-man-with-hands-crossed-chest_176420-18743.jpg?size=626&ext=jpg",
+  },
+  {
+    serial: 1,
+    name: "kalam",
+    age: 25,
+    relation: "sister",
+    phone: "39843948793749",
+    photo:
+      "https://img.freepik.com/free-photo/handsome-confident-smiling-man-with-hands-crossed-chest_176420-18743.jpg?size=626&ext=jpg",
+  },
+  {
+    serial: 1,
+    name: "Sajjad",
+    age: 20,
+    relation: "brother",
+    phone: "39843948793749",
+    photo:
+      "https://image.freepik.com/free-photo/happy-confident-customer-recommending-new-product_1262-18240.jpg",
+  },
+  {
+    serial: 1,
+    name: "Hossain",
+    age: 27,
+    relation: "father",
+    phone: "39843948793749",
+    photo:
+      "https://img.freepik.com/free-photo/handsome-confident-smiling-man-with-hands-crossed-chest_176420-18743.jpg?size=626&ext=jpg",
+  },
+  {
+    serial: 1,
+    name: "kalam",
+    age: 25,
+    relation: "sister",
+    phone: "39843948793749",
+    photo:
+      "https://img.freepik.com/free-photo/handsome-confident-smiling-man-with-hands-crossed-chest_176420-18743.jpg?size=626&ext=jpg",
+  },
+];
