57e54d6436a1e098a378e37d29a709658bf884ec
Sajjad <sajjadhossainctg26@gmail.com>
2021-12-29 16:43:59 +0600

    submit

--- class12/app.js
+++ class12/app.js
@@ -1,10 +1,44 @@
+// const name = document.querySelector("#name");
+// const year = document.querySelector("#year");
+// const result = document.querySelector("#result");
+// const send = document.querySelector("#send");
+
+// send.addEventListener("click", () => {
+//   if (name.value == "" || year.value == "") {
+//     result.innerHTML = <p class="alert alert-danger">All field are required</p>;
+//   } else {
+//     result.innerHTML = agecal(name.value, year.value);
+//   }
+// });
+
+// const name1 = document.querySelector("#name1");
+// const year1 = document.querySelector("#year1");
+// const result1 = document.querySelector("#result1");
+// const send1 = document.querySelector("#send1");
+// const gender = document.querySelector("#gender");
+// const msg = document.querySelector("#msg");
+
+// send1.addEventListener("click", () => {
+//   let pattern = /^male|female$/;
+//   if (pattern.test(gender.value) == false) {
+//     msg.innerHTML = "please put male or female";
+//     gender.style.border = "1px solid red";
+//   } else {
+//     msg.innerHTML = "";
+//     gender.style.border = "1px solid black";
+//   }
+// });
 const name = document.querySelector("#name");
 const year = document.querySelector("#year");
 const result = document.querySelector("#result");
 const send = document.querySelector("#send");
 
 send.addEventListener("click", () => {
-  result.innerHTML = agecal(name.value, year.value);
+  if (name.value == "" || year.value == "") {
+    result.innerHTML = `<p class="alert alert-danger">All field are required</p>`;
+  } else {
+    result.innerHTML = agecal(name.value, year.value);
+  }
 });
 
 const name1 = document.querySelector("#name1");
@@ -23,5 +57,9 @@ send1.addEventListener("click", () => {
     msg.innerHTML = "";
     gender.style.border = "1px solid black";
   }
-  result1.innerHTML = bibahoage(name1.value, year1.value, gender.value);
+  if (name1.value == "" || year1.value == "" || gender.value == "") {
+    result1.innerHTML = `<p class="alert alert-danger">All field are required</p>`;
+  } else {
+    result1.innerHTML = bibahoage(name1.value, year1.value, gender.value);
+  }
 });
