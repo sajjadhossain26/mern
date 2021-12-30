ebb07e31afb6377e78f5e9b4835d67e420eed310
Sajjad <sajjadhossainctg26@gmail.com>
2021-12-30 10:52:35 +0600

    submit

--- class12/app.js
+++ class12/app.js
@@ -1,33 +1,3 @@
-// const name = document.querySelector("#name");
-// const year = document.querySelector("#year");
-// const result = document.querySelector("#result");
-// const send = document.querySelector("#send");
-
-// send.addEventListener("click", () => {
-//   if (name.value == "" || year.value == "") {
-//     result.innerHTML = <p class="alert alert-danger">All field are required</p>;
-//   } else {
-//     result.innerHTML = agecal(name.value, year.value);
-//   }
-// });
-
-// const name1 = document.querySelector("#name1");
-// const year1 = document.querySelector("#year1");
-// const result1 = document.querySelector("#result1");
-// const send1 = document.querySelector("#send1");
-// const gender = document.querySelector("#gender");
-// const msg = document.querySelector("#msg");
-
-// send1.addEventListener("click", () => {
-//   let pattern = /^male|female$/;
-//   if (pattern.test(gender.value) == false) {
-//     msg.innerHTML = "please put male or female";
-//     gender.style.border = "1px solid red";
-//   } else {
-//     msg.innerHTML = "";
-//     gender.style.border = "1px solid black";
-//   }
-// });
 const name = document.querySelector("#name");
 const year = document.querySelector("#year");
 const result = document.querySelector("#result");
@@ -43,6 +13,9 @@ send.addEventListener("click", () => {
   year.value = "";
 });
 
+/**
+ * bibaho calculator
+ */
 const name1 = document.querySelector("#name1");
 const year1 = document.querySelector("#year1");
 const result1 = document.querySelector("#result1");
@@ -67,4 +40,18 @@ send1.addEventListener("click", () => {
   name1.value = "";
   year1.value = "";
   gender.value = "";
+});
+
+/**
+ * currency converter
+ */
+const amount = document.querySelector("#amount");
+const getamount = document.querySelector("#getamount");
+const convert = document.querySelector("#convert");
+const convertResult = document.querySelector("#convertResult");
+
+convert.addEventListener("click", () => {
+  convertResult.innerHTML = `Now your current amount ${
+    getamount.value * amount.value
+  } taka`;
 });
