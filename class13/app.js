31fb2736fdc0536ca4b2a9729710863c8a26f4d1
Sajjad <sajjadhossainctg26@gmail.com>
2022-01-09 12:53:58 +0600

    submit

--- class13/app.js
+++ class13/app.js
@@ -9,13 +9,26 @@ setInterval(() => {
   let h = date.getHours();
   let m = date.getMinutes();
   let s = date.getSeconds();
+
   if (h == 0) {
     h = 1;
   } else {
     h = date.getHours();
   }
   clockH.innerHTML = `${h > 12 ? zero(h - 12) : zero(h)}`;
-  clockM.innerHTML = `: ${zero(m)}`;
+  clockM.innerHTML = `:${zero(m)}`;
   clockS.innerHTML = `${zero(s)}`;
   clockAMPM.innerHTML = `${h >= 12 ? "PM" : "AM"}`;
 }, 1000);
+
+const show = document.querySelector(".date");
+let date = new Date();
+let day = date.getDate();
+let year = date.getFullYear();
+let month = date.getMonth();
+if (month == 0) {
+  month += 1;
+} else {
+  month = date.getMonth();
+}
+show.innerHTML = `${zero(month)}-${zero(day)}-${zero(year)}`;
