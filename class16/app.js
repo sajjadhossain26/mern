16881dd8144655d88dec2d79b4ce4979d4269c86
Sajjad <sajjadhossainctg26@gmail.com>
2022-02-06 22:39:30 +0600

    submit

--- class16/app.js
+++ class16/app.js
@@ -38,3 +38,24 @@ modal.addEventListener("click", function (e) {
     this.classList.remove("active");
   }
 });
+// accordion
+
+const accordionH = document.querySelectorAll(".accordion-header");
+
+accordionH.forEach((item) => {
+  item.addEventListener("click", function () {
+    accordionH.forEach((data) => {
+      if (data !== this) {
+        data.classList.remove("active");
+        data.nextElementSibling.style.height = "0px";
+      }
+    });
+    item.classList.toggle("active");
+    if (item.classList.contains("active")) {
+      item.nextElementSibling.style.height =
+        item.nextElementSibling.scrollHeight + "px";
+    } else {
+      item.nextElementSibling.style.height = "0px";
+    }
+  });
+});
