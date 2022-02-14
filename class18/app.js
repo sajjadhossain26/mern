1e2a4772b0b97e259ac674d16092d2eba40c4660
Sajjad <sajjadhossainctg26@gmail.com>
2022-02-14 21:10:23 +0600

    Upload data

---
+++ class18/app.js
@@ -0,0 +1,59 @@
+const devs = document.querySelector(".devs");
+fetch("http://localhost:2020/devs")
+  .then((data) => data.json())
+  .then((data) => {
+    let devs_data = "";
+    data.map((data) => {
+      devs_data += `
+          <div class="col-md-3 my-3">
+          <div class="card shadow">
+          <a href="#" onclick="single_devs(${data.id})" class="w-100" data-bs-toggle="modal" data-bs-target="#student_modal" >  <img style="object-fit: cover;"  class="w-100"
+              src="${data.photo}"
+              alt=""
+            /></a>
+            <div class="card-body">
+              <h3>${data.name}</h3>
+               <span>${data.skill}</span><br>
+                 
+               <span>Age: ${data.age}</span>
+            </div>
+          </div>
+        </div>
+          
+          
+          `;
+    });
+    devs.innerHTML = devs_data;
+  });
+function single_devs(id) {
+  const modal_data = document.querySelector(".modal-data");
+  modal_data.innerHTML = "";
+  fetch("http://localhost:2020/devs/" + id)
+    .then((data) => data.json())
+    .then((data) => {
+      modal_data.innerHTML = `
+      <div class="single_devs_data">
+       <div class="row">
+                <div class="col-md-6 p-0">
+                  <img class="w-100"
+                    src="${data.photo}"
+                    alt=""
+                  />
+                </div>
+                <div class="col-md-6 p-5">
+                  <h3>${data.name}</h3>
+                  <h3>Age: ${data.age}</h3>
+                  <h3>Location: ${data.location}</h3>
+                  <h3>Skill: ${data.skill}</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Temporibus, quaerat!
+                  </p>
+                </div>
+              </div>
+      </div>
+      
+
+      `;
+    });
+}
